5a125aef9497e45ef9d8e0f0daa40490d0c09a4b
Shawal Mbalire <mbalireshawal@gmail.com>
2023-09-06 03:52:23 +0300

    Create project1.c

---
+++ project1/project1.c
@@ -0,0 +1,11 @@
+// Controlling arduino with bare metal programming
+// Port B = [7,6,5,4,3,2,1,0]
+#include <avr/io.h>
+#include <util/delay.h> //paru -S avr-libc avrdude gcc-avr binutils-avr
+
+void main(){
+
+    // led built in is on port B pin 5
+
+    return 0;
+}
